a440932f17ded76e2e1b39ed740287ed5bd21392
Ariel Jatib <ariel.jatib@gmail.com>
2014-07-28 09:08:24 -0400

    added json test data and updated controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -5,9 +5,19 @@
 
 var benbenControllers = angular.module('benbenControllers',[]);
 
-benben.controller('homeCtrl', [function(){
-    console.log('home skillet!')
-}]);
+benben.controller('homeCtrl', function($scope, $http){
+    console.log('let\'s load some data!');
+    uri = '/data/test.json';
+    $http.get(uri, $scope.lorem)
+    	.success(function(data){
+    		console.log('grabbin some data')
+    		$scope.lorem = data;
+    		console.log('got the data', $scope.lorem);
+    	}).
+    	error(function(){
+    		console.log('oops - something went wrong')
+    	})
+});
 
 benben.controller('themeCtrl', [function(){
     console.log('theme')
